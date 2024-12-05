efa7fbd461db1bb5701a0b3c24c51f1906b36f6b
Ann Kuts <annkuts@Anns-MacBook-Pro.local>
2024-12-05 14:57:48 +0200

    made the 9th task

--- Exercises/9-array.js
+++ Exercises/9-array.js
@@ -1,16 +1,14 @@
-'use strict';
+"use strict";
 
-/* Collections: Array, Hash (Object)
-
-Implement phone book using array of records.
-- Define Array of objects with two fields: `name` and `phone`.
-Object example: `{ name: 'Marcus Aurelius', phone: '+380445554433' }`.
-- Implement function `findPhoneByName` with signature
-`findPhoneByName(name: string): string`. Returning phone from that object
-where field `name` equals argument `name`. Use `for` loop for this search. */
-
-const phonebook = null;
-
-const findPhoneByName = null;
+const phonebook = [
+{name: "Anna", phone: "+380333333333"},
+{name: "Ivanka", phone: "+380111111111"},
+{name: "Artem", phone: "+380999999999"}
+];
+const findPhoneByName = (name) => {
+  for (const obj of phonebook) {
+    if (obj.name === name) return obj.phone;
+  }
+};
 
 module.exports = { phonebook, findPhoneByName };
