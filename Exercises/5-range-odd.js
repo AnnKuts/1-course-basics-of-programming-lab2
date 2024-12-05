01af6d013beb8af8b0a1e7a34ffb249724930de1
Ann Kuts <annkuts@Anns-MacBook-Pro.local>
2024-12-05 14:25:20 +0200

    made the 6th task and some changes in other tasks

--- Exercises/5-range-odd.js
+++ Exercises/5-range-odd.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 const rangeOdd = (start, end) => {
   const length = [];
@@ -9,5 +9,5 @@ const rangeOdd = (start, end) => {
   }
   return length;
 };
-
+console.log(rangeOdd(15,30));
 module.exports = { rangeOdd };
--- Exercises/6-calculate.js
+++ Exercises/6-calculate.js
@@ -1,27 +1,14 @@
-'use strict';
-
-/* Call function from function in loop
-- Implement function `average` with signature
-  `average(a: number, b: number): number`
-  calculating average (arithmetic mean).
-- Implement function `square` with signature
-  `square(x: number): number` calculating square of x.
-- Implement function `cube` with signature
-  `cube(x: number): number` calculating cube of x.
-- Call `square` and `cube` in loop 0 to 9, pass results
-  to function `average` on each iteration.
-  Add calculation results to array and return this array
-  from function `calculate`.
-
-Call functions `square` and `cube` in loop, then pass their
-results to function `average`. Print what `average` returns. */
-
-const square = null;
-
-const cube = null;
-
-const average = null;
-
-const calculate = null;
+"use strict";
+const average = (a, b) => (a + b) / 2;
+const square = (a) => a * a;
+const cube = (a) => a ** 3;
+const calculate = () => {
+  const array = [];
+  for (let i = 0; i <= 9; i++) {
+    const x = average(square(i), cube(i));
+    array.push(x);
+  }
+  return array;
+};
 
 module.exports = { square, cube, average, calculate };
